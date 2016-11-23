537557245fa28eaf8861a277144ec30f043d1a43
Xu Mengxiang <834489218@qq.com>
2016-11-23 21:19:31 +0800

    Add Location Method & Add Materials and Utils

--- app/src/main/java/com/xmx/androidmapbase/BaseMap/MapActivity.java
+++ app/src/main/java/com/xmx/androidmapbase/BaseMap/MapActivity.java
@@ -1,25 +1,56 @@
 package com.xmx.androidmapbase.BaseMap;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Circle;
+import com.amap.api.maps.model.CircleOptions;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.xmx.androidmapbase.R;
 import com.xmx.androidmapbase.Tools.ActivityBase.BaseActivity;
+import com.xmx.androidmapbase.Tools.MapUtils.util.SensorEventHelper;
 
-public class MapActivity extends BaseActivity {
-    MapView mMapView = null;
-    private AMap aMap;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_map)
+public class MapActivity extends BaseActivity implements LocationSource {
+
+    @ViewInject(R.id.map)
+    private MapView mMapView;
+
+    private AMap mAMap;
+    private AMapLocationClient mLocationClient; //定位器
+    private OnLocationChangedListener mListener; //定位监听器
+
+    private static final int STROKE_COLOR = Color.argb(180, 3, 145, 255); //精度边缘颜色
+    private static final int FILL_COLOR = Color.argb(0, 128, 192, 192); //精度填充颜色
+    private boolean mFirstFix = false; //是否已将图标添加到地图
+    private Marker mLocMarker; //定位点
+    private Circle mCircle; //精度圆
+    private SensorEventHelper mSensorHelper; //用于获取指南针方向
+    public static final String LOCATION_MARKER_FLAG = "myLocation";
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_map);
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.map);
-        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
         mMapView.onCreate(savedInstanceState);
-        if (aMap == null) {
-            aMap = mMapView.getMap();
+        if (mAMap == null) {
+            mAMap = mMapView.getMap();
         }
         //aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
     }
@@ -31,34 +62,131 @@ public class MapActivity extends BaseActivity {
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
+        mAMap.setLocationSource(this);//设置定位监听
+        mAMap.getUiSettings().setMyLocationButtonEnabled(false);//设置默认定位按钮是否显示
+        mAMap.setMyLocationEnabled(true);//设置为true表示可触发定位
+        mAMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);//设置定位的类型为定位模式
 
+        mSensorHelper = new SensorEventHelper(this);
+        mSensorHelper.registerSensorListener();
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null) {
+            mLocationClient = new AMapLocationClient(this);
+            AMapLocationClientOption locationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mLocationClient.setLocationListener(new AMapLocationListener() {
+                @Override
+                public void onLocationChanged(AMapLocation aMapLocation) {
+                    if (mListener != null && aMapLocation != null) {
+                        if (aMapLocation.getErrorCode() == 0) {
+                            LatLng location = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+                            if (!mFirstFix) {
+                                mFirstFix = true;
+                                addCircle(location, aMapLocation.getAccuracy());//添加定位精度圆
+                                addMarker(location);//添加定位图标
+                                mSensorHelper.setCurrentMarker(mLocMarker);//定位图标旋转
+                                mAMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+                            } else {
+                                mCircle.setCenter(location); //精度中心为定位点
+                                mCircle.setRadius(aMapLocation.getAccuracy()); //精度半径
+                                mLocMarker.setPosition(location); //定位点
+                            }
+                            //mListener.onLocationChanged(aMapLocation); //系统默认定位事件
+
+                        } else {
+                            String errText = "定位失败," + aMapLocation.getErrorCode() + ": " + aMapLocation.getErrorInfo();
+                            showToast(errText);
+                            showLog("AMapErr", errText);
+                        }
+                    }
+                }
+            });
+            //设置为高精度定位模式
+            locationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mLocationClient.setLocationOption(locationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mLocationClient.startLocation();
+        }
+    }
+
+    private void addCircle(LatLng latlng, double radius) {
+        CircleOptions options = new CircleOptions();
+        options.strokeWidth(1f);
+        options.fillColor(FILL_COLOR);
+        options.strokeColor(STROKE_COLOR);
+        options.center(latlng);
+        options.radius(radius);
+        mCircle = mAMap.addCircle(options);
+    }
+
+    private void addMarker(LatLng latlng) {
+        if (mLocMarker != null) {
+            return;
+        }
+        Bitmap bMap = BitmapFactory.decodeResource(this.getResources(),
+                R.drawable.navi_map_gps_locked);
+        BitmapDescriptor des = BitmapDescriptorFactory.fromBitmap(bMap);
+//		BitmapDescriptor des = BitmapDescriptorFactory.fromResource(R.drawable.navi_map_gps_locked);
+        MarkerOptions options = new MarkerOptions();
+        options.icon(des);
+        options.anchor(0.5f, 0.5f);
+        options.position(latlng);
+        mLocMarker = mAMap.addMarker(options);
+        mLocMarker.setTitle(LOCATION_MARKER_FLAG);
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
         mMapView.onResume();
+        if (mSensorHelper != null) {
+            mSensorHelper.registerSensorListener();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        if (mSensorHelper != null) {
+            mSensorHelper.unRegisterSensorListener();
+            mSensorHelper.setCurrentMarker(null);
+            mSensorHelper = null;
+        }
         mMapView.onPause();
+        deactivate();
+        mFirstFix = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+        if (null != mLocationClient) {
+            mLocationClient.onDestroy();
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
         mMapView.onSaveInstanceState(outState);
     }
 }
