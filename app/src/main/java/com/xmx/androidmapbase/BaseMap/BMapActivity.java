b10cf065846ed7d3699da556346c4d094ab45914
Xu Mengxiang <834489218@qq.com>
2016-12-15 13:56:46 +0800

    Add BaseMapActivity for BaiduMap

--- app/src/main/java/com/xmx/androidmapbase/BaseMap/BMapActivity.java
+++ app/src/main/java/com/xmx/androidmapbase/BaseMap/BMapActivity.java
@@ -2,22 +2,27 @@ package com.xmx.androidmapbase.BaseMap;
 
 import android.os.Bundle;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
 import com.xmx.androidmapbase.R;
 import com.xmx.androidmapbase.Tools.ActivityBase.BaseActivity;
+import com.xmx.androidmapbase.Tools.Map.BMap.Activity.BaseMapActivity;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_bmap)
-public class BMapActivity extends BaseActivity {
+public class BMapActivity extends BaseMapActivity {
 
-    @ViewInject(R.id.bmapView)
-    MapView mMapView;
+    @Override
+    protected void getMapView() {
+        mMapView = getViewById(R.id.bmapView);
+    }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        mMapView.onCreate(this, savedInstanceState);
+        super.initView(savedInstanceState);
     }
 
     @Override
@@ -27,30 +32,22 @@ public class BMapActivity extends BaseActivity {
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
+        mBMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        mBMap.setTrafficEnabled(true);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
+    protected void whenResume() {
+
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
+    protected void whenPause() {
+
+    }
+
+    @Override
+    protected void whenDestroy() {
+
     }
 }
---
+++ app/src/main/java/com/xmx/androidmapbase/Tools/Map/BMap/Activity/BaseMapActivity.java
@@ -0,0 +1,75 @@
+package com.xmx.androidmapbase.Tools.Map.BMap.Activity;
+
+import android.os.Bundle;
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+import com.xmx.androidmapbase.Tools.ActivityBase.BaseActivity;
+
+/**
+ * Created by The_onE on 2015/12/27.
+ */
+public abstract class BaseMapActivity extends BaseActivity {
+
+    protected MapView mMapView;
+    protected BaiduMap mBMap;
+    public static final float DEFAULT_SCALE = 15; //默认缩放比例
+
+    protected abstract void getMapView();
+    protected abstract void whenResume();
+    protected abstract void whenPause();
+    protected abstract void whenDestroy();
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        getMapView();
+        mMapView.onCreate(this, savedInstanceState);
+        if (mBMap == null) {
+            mBMap = mMapView.getMap();
+        }
+    }
+
+    protected void focusLocation(LatLng location) {
+        focusLocation(location, DEFAULT_SCALE);
+    }
+
+    //聚焦定位点，缩放比越大放大程度越高，范围3-21
+    protected void focusLocation(LatLng location, float scale) {
+        MapStatusUpdate mapStatusUpdate
+                = MapStatusUpdateFactory.zoomTo(scale);
+        mBMap.setMapStatus(mapStatusUpdate);
+        mapStatusUpdate
+                = MapStatusUpdateFactory.newLatLng(location);
+        mBMap.setMapStatus(mapStatusUpdate);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+        whenResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+        whenPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+        whenDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+}
